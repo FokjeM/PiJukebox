765354f167e20d1194a35b424fd53d1c33498fe4
Ruben <thewheelz14@gmail.com>
2018-12-13 12:28:25 +0100

    Added iron-ajax and made a basic tracklist

--- Frontend/src/elements/track-list.js
+++ Frontend/src/elements/track-list.js
@@ -1,5 +1,13 @@
 import {PolymerElement, html} from '@polymer/polymer/polymer-element.js';
 import './../shared-styles.js';
+import '@polymer/polymer/lib/elements/dom-repeat.js';
+import '@polymer/iron-ajax/iron-ajax.js';
+
+import '@polymer/paper-button/paper-button.js';
+import '@polymer/paper-icon-button/paper-icon-button.js';
+import '@polymer/iron-icon/iron-icon.js';
+import '@polymer/iron-icons/iron-icons.js';
+import '@polymer/iron-icons/av-icons.js';
 
 class TrackList extends PolymerElement {
   static get template() {
@@ -17,16 +25,31 @@ class TrackList extends PolymerElement {
         .track-info {
           display: flex;
           flex-direction: column;
-          align-items: center;
+          align-items: flex-start;
         }
       </style>
       
+      <iron-ajax
+        auto
+        url="http://localhost:8080/api/v1/laptops/"
+        handle-as="json"
+        last-response="{{response}}">
+      </iron-ajax>
+      
       <div class="container">
-        
+          
         <div class="track-info">
-          HERE GOES THE TRACK LIST
-          <!--<p>[[trackName]]</p>-->
-          <!--<p>[[trackArtist]]</p>-->
+          <h1>Tracks</h1>
+            <dom-repeat items="{{response}}" as="track">
+              <template>
+                <div class="track-info">
+                  <div>
+                    <paper-icon-button icon="av:play-circle-outline"></paper-icon-button>
+                    [[track.id]]: [[track.name]]
+                  </div>
+                </div>
+              </template>
+            </dom-repeat>
         </div>
           
       </div>
