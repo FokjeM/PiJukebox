e399c30bed0bfcf863e65f82a206d05cb090951a
Fokje Moer <Pretender0906@gmail.com>
2018-12-16 22:30:47 +0100

    Set up Player and added an empty Playlist class against errors

--- PiJukebox/src/main/java/com/PiJukebox/Player.java
+++ PiJukebox/src/main/java/com/PiJukebox/Player.java
@@ -1,2 +1,24 @@
 package com.PiJukebox;
+import java.util.*;
 
+public class Player {
+    
+    /*
+     * This class is supposed to handle the playing of files.
+     * It exposes a few methods that leave little to no room for external mutations.
+    */
+    
+    private List playlist;
+    private boolean repeat;
+    private boolean repeatOne;
+    
+    /*
+     * @param rep Sets initial repeat on/off
+     * @param shuffle Sets wether or not to shuffle the playlist initially
+     * @param list the playlist to play. This moves into a list to prevent
+     *      accidentally messing up the DB entry for the Playlist
+    */
+    public Player(boolean rep, boolean shuffle, Playlist list) {
+        
+    }
+}
