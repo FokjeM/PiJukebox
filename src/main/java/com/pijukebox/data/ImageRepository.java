fdc40cd69e28eb1a9d3d2d9616df1ad84e249b50
codrut51 <codrutsoft13@gmail.com>
2018-12-19 12:09:27 +0100

    Deleted some files

--- src/main/java/com/pijukebox/data/ImageRepository.java
+++
@@ -1,5 +0,0 @@
-package com.pijukebox.data;
-
-public interface ImageRepository {
-    InterImage getImageById(Long id);
-}
--- src/main/java/com/pijukebox/data/jpa/SpringJpaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.pijukebox.data.jpa;
-
-import com.pijukebox.data.model.AnnotatedImage;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SpringJpaRepository extends JpaRepository<AnnotatedImage, Long> {
-}
