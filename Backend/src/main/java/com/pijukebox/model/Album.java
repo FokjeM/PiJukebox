b0f3d8ae3ec1501ea64ece46218d9c25b27bacae
codrut51 <codrutsoft13@gmail.com>
2019-01-02 17:03:31 +0100

    Artist, Genre, Album

--- Backend/src/main/java/com/pijukebox/model/Album.java
+++ Backend/src/main/java/com/pijukebox/model/Album.java
@@ -14,7 +14,14 @@ public class Album {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name="id")
     private Long id;
+
+    @Column(name="name")
+    private String name;
+
+    @Column(name="releaseDate")
+    private String releaseDate;
 
     // https://stackoverflow.com/questions/5478328/jpa-jointable-annotation
     @JoinTable
--- Backend/src/main/java/com/pijukebox/repository/IAlbumRepository.java
+++ Backend/src/main/java/com/pijukebox/repository/IAlbumRepository.java
@@ -1,4 +1,11 @@
 package com.pijukebox.repository;
 
+import com.pijukebox.model.Album;
+
+import java.util.List;
+
 public interface IAlbumRepository {
+    List<Album> findAll();
+
+    Album getById(Long id);
 }
