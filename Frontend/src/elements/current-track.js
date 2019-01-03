1cc430b98b9a4e978d5e37d9249c32c77156b6ef
Ruben <thewheelz14@gmail.com>
2019-01-03 12:46:52 +0100

    Added a call to get the current track

--- Frontend/src/elements/current-track.js
+++ Frontend/src/elements/current-track.js
@@ -1,4 +1,6 @@
 import {PolymerElement, html} from '@polymer/polymer/polymer-element.js';
+import '@polymer/iron-ajax/iron-ajax.js';
+
 import './../shared-styles.js';
 
 class CurrentTrack extends PolymerElement {
@@ -20,14 +22,26 @@ class CurrentTrack extends PolymerElement {
           align-items: center;
         }
       </style>
-      
+
+      <iron-ajax
+        auto
+        url="http://localhost:8080/api/v1/laptops/"
+        handle-as="json"
+        last-response="{{currentTrack}}">
+      </iron-ajax>
+
       <div class="container">
         
         <div class="trackInfo">
-          <p>[[trackName]]</p>
-          <p>[[trackArtist]]</p>
+          <p>[[currentTrack.trackName]]</p>
+          <p>[[currentTrack.trackArtist]]</p>
         </div>
-          
+
+        <!-- <div class="trackInfo"> -->
+          <!-- <p>[[trackName]]</p> -->
+          <!-- <p>[[trackArtist]]</p> -->
+        <!-- </div> -->
+
       </div>
     `;
   }
