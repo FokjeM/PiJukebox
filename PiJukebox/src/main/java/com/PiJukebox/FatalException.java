94d8fe00794ffc090a1cad42385bcc7d6d962b06
Fokje Moer <Pretender0906@gmail.com>
2019-01-07 15:20:40 +0100

    Continued development; pushing at more than 1mb/s Added Fatal and Non-fatal exceptions

---
+++ PiJukebox/src/main/java/com/PiJukebox/FatalException.java
@@ -0,0 +1,27 @@
+package com.PiJukebox;
+
+/**
+ * An exception that poses a valid reason to halt execution of the program
+ * @author Riven
+ */
+class FatalException extends Exception {
+    
+    /**
+     * Fatal Exception with all parameters
+     */
+    protected FatalException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message + "\r\n\tCould not recover from this error.\r\n\tEXECUTION WILL NOW HALT!", cause, enableSuppression, writableStackTrace);
+    }
+    
+    public FatalException(String message, Throwable cause) {
+        this(message + "\r\n\tCould not recover from this error.\r\n\tEXECUTION WILL NOW HALT!", cause, false, false);
+    }
+    
+    public FatalException(Throwable cause) {
+        this("No message was given, this is NOT a non-fatal error.\r\nEXECUTION WILL NOW HALT!", cause, false, false);
+    }
+    
+    public FatalException() {
+        super("No message or cause was given. This error is regarded as FATAL.\r\nEXECUTION WILL NOW HALT!");
+    }
+}
--- PiJukebox/src/main/java/com/PiJukebox/Track.java
+++ PiJukebox/src/main/java/com/PiJukebox/Track.java
@@ -5,7 +5,7 @@ import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
-import com.mpatric.mp3agic;
+import com.mpatric.mp3agic.*;
 
 /**
  * Implementation of a Track
@@ -14,7 +14,7 @@ import com.mpatric.mp3agic;
  * - the path of the track
  * - the metadata of the track
  * 
- * Will use the MP3agick library to read the metadata
+ * Will use the mp3agic library to read the metadata
  * @author Riven
  */
 class Track {
@@ -23,10 +23,25 @@ class Track {
     private final String title;
     private final int bitrate;
     private final String filetype;
-    private static mp3agic.Mp3File mp3;
+    private final FileWrapper mp3file;
     
-    public Track(){
-        filepath = null;
+    /**
+     * Create a Track object with info from the database and application
+     * @param mediaDir The general media directory for PiJukebox.
+     *          The application must provide this
+     * @param filename The filename of the Track, this should come from the DB
+     * @throws NonFatalException Something went wrong, but we can recover.
+     */
+    public Track(String mediaDir, String filename) throws NonFatalException{
+        filepath = FileSystems.getDefault().getPath(mediaDir, filename);
+        try {
+            //Wrap the file, the library handles the hardships
+            mp3file = new FileWrapper(filepath);
+        } catch (NullPointerException e) {
+            throw new NonFatalException("Path was set to NULL; message given:\r\n" + e.getMessage(), e, false, false);
+        } catch (IOException e) {
+            throw new NonFatalException("File doesn't exist; message given:\r\n" + e.getMessage(), e, false, false);
+        }
         title = "";
         bitrate = 0;
         filetype = "";
