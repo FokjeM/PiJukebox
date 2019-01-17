592398cb6de40b628c7cd88edc1bb5f6352efcfd
Fokje Moer <Pretender0906@gmail.com>
2019-01-17 11:00:22 +0100

    A few code improvements, repeat toggles, getters and some magic

--- PiJukebox/src/main/java/com/PiJukebox/Player.java
+++ PiJukebox/src/main/java/com/PiJukebox/Player.java
@@ -8,6 +8,7 @@ import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Scanner;
 
@@ -29,16 +30,22 @@ public class Player {
      * directory as this class or its corresponding jar/war file
      */
     private final static Path QUEUE_FILE = FileSystems.getDefault().getPath("stateful_queue.out");
-    
+    /**
+     * The ErrorLogger object for this Player.
+     * 
+     * When recovering from a previous instance, the ErrorLogger from that
+     * instance should be passed as the ErrorLogger argument 
+     */
+    public final ErrorLogger log;
     private final Queue queue;
     private List statefulQueue;
-    public ErrorLogger log;
     private Track currentTrack;
     private int trackNum;
     private boolean repeat;
     private boolean repeatOne;
     private Process proc;
     private boolean procKill;
+    //You can't be playing when you start
     private boolean playing = false;
     private final Thread procExitCode;
     private final Thread procOutput;
@@ -46,6 +53,9 @@ public class Player {
     
     /**
      * Instantiate a new Player. This constructor sets no default options.
+     * 
+     * If recovering from a previous instance, do NOT pass a new ErrorLogger,
+     * instead pass the ErrorLogger from that instance of Player.
      * <ul>
      * <li>Repeat: ON (true) or OFF (false) depending on rep</li>
      * <li>Repeat One: ON (true) or OFF (false) depending on repOne</li>
@@ -53,8 +63,10 @@ public class Player {
      * @param rep Set repeating the entire Queue indefinitely.
      * @param repOne Set repeating this Track indefinitely.
      * @param q The Queue to instantiate with.
+     * @param el the ErrorLogger for this instance of Player.
      */
-    public Player(boolean rep, boolean repOne, Queue q) {
+    public Player(boolean rep, boolean repOne, Queue q, ErrorLogger el){
+        log = el;
         queue = new Queue(q);
         currentTrack = null;
         trackNum = 0;
@@ -91,15 +103,40 @@ public class Player {
             while((scan.hasNextLine() || playing || proc.isAlive()) && !procKill){
                 log.writeFFPlayLog(scan.next());
             }   try {
-                Player.this.onSongEnd();
+                this.onSongEnd();
             } catch (NonFatalException | FatalException ex) {
                 this.writeLog(ex);
             }
         });
-        
-        
     }
     
+    /**
+     * Instantiate a new Player. This constructor sets no default options.
+     * 
+     * Creates a new instance of {@link com.PiJukebox.ErrorLogger ErrorLogger} to log this object.
+     * This logger should be retrieved by the instantiating class to pass to the
+     * constructor in case of a recovery.
+     * <ul>
+     * <li>Repeat: ON (true) or OFF (false) depending on rep</li>
+     * <li>Repeat One: ON (true) or OFF (false) depending on repOne</li>
+     * </ul>
+     * @param rep Set repeating the entire Queue indefinitely.
+     * @param repOne Set repeating this Track indefinitely.
+     * @param q The Queue to instantiate with.
+     */
+    public Player(boolean rep, boolean repOne, Queue q) {
+        this(rep, repOne, q, new ErrorLogger(LocalDateTime.now()));
+    }
+    
+    /**
+     * Instantiate a new Player. This constructor sets no default options.
+     * <ul>
+     * <li>Repeat: ON (true) or OFF (false) depending on rep</li>
+     * <li>Repeat One: ON (true) or OFF (false) depending on repOne</li>
+     * </ul>
+     * @param rep Set repeating the entire Queue indefinitely.
+     * @param repOne Set repeating this Track indefinitely.
+     */
     public Player(boolean rep, boolean repOne){
         this(rep, repOne, new Queue());
     }
@@ -203,8 +240,9 @@ public class Player {
     /**
      * Plays the next song in queue using {@link #playTrack(com.PiJukebox.Track) playTrack}.
      * @throws FatalException propagated from {@link  #playTrack(com.PiJukebox.Track) playTrack}
+     * @throws com.PiJukebox.NonFatalException propagated from {@link #playTrack(com.PiJukebox.Track) playTrack}
      */
-    public void next() throws FatalException{
+    public void next() throws FatalException, NonFatalException{
         if(trackNum == 0){
             //As long as there is still a track in there, we'll play it.
             while(!queue.containsKey(trackNum) && trackNum <= queue.size()){
@@ -231,57 +269,91 @@ public class Player {
         playTrack(currentTrack);
     }
     
-    public void previous() throws FatalException {
-        if(trackNum > 0) {
+    /**
+     * Moves back to the previous Track, or wraps back to the last when repeating.
+     * @throws FatalException propagated from {@link #playTrack(com.PiJukebox.Track) playTrack}
+     * @throws NonFatalException propagated from {@link #playTrack(com.PiJukebox.Track) playTrack}
+     */
+    public void previous() throws FatalException, NonFatalException {
+        if(trackNum > 1) {
             //pre-decrement. Forces the JVM to decrement before doing anything else
             currentTrack = queue.get(--trackNum);
-            updateStatefulQueue();
-            playTrack(currentTrack);
+        } else if(!repeat) {
+            //Don't do ANYTHING we don't need to do.
+            return;
+        } else {
+            trackNum = queue.size();
+            currentTrack = queue.get(trackNum);
         }
+        updateStatefulQueue();
+        playTrack(currentTrack);
     }
     
     /**
      * Plays the specified Track
      * @param t the Track to play
      * @throws com.PiJukebox.FatalException When FFPlay can't be started
+     * @throws com.PiJukebox.NonFatalException propagated from {@link #stop() stop()}
      */
-    public void playTrack(Track t) throws FatalException{
-        procKill = true;
+    public void playTrack(Track t) throws FatalException, NonFatalException{
+        stop(); //Make sure we stop playing so we can do our standard set of tasks
         updateStatefulQueue();
+        //Setup the arguments we need for this Track
         StringBuilder cmd = new StringBuilder();
-        cmd.append("-nostats -autoexit -nodisp -vn -sn ");
-        if(repeatOne){
-            cmd.append("-loop 0 ");
-        }
-        cmd.append("-codec:a "); cmd.append(t.getStreamType());
-        cmd.append(" -t "); cmd.append(t.getDuration());
-        cmd.append(" -i "); cmd.append(t.getPath().toAbsolutePath().toString());
+        //Don't print file info, exit when done and don't display anything
+        cmd.append("-nostats -autoexit -nodisp ");
+        //Disable video and subtitles
+        cmd.append("-vn -sn ");
+        //Set the codec for all streams
+        cmd.append("-codec:a ");
+        cmd.append(t.getStreamType()); //Get the encoding, thus codec for this Track
+        //Set the track duration so we can tell FFPlay how long it should run
+        cmd.append(" -t ");
+        cmd.append(t.getDuration());//Get the duration for this Track
+        //Set the file we want to play
+        cmd.append(" -i ");
+        cmd.append(t.getPath().toAbsolutePath().toString()); //Get the full, absolute path
         try {
-            proc = new ProcessBuilder("ffplay", cmd.toString()).redirectErrorStream(true).start();
+            //the process shouldn't get killed on starting
             procKill = false;
+            //ProcessBuilder expects the executable or command to be seperate from all arguments
+            proc = new ProcessBuilder("ffplay", cmd.toString()).redirectErrorStream(true).start();
+            //start listening for the exit code and any output without hanging
             procExitCode.start();
             procOutput.start();
         } catch (IOException ex) {
+            procKill = true; //Stop the threads again
             throw new FatalException("Could not start FFPlay!", ex);
         }
         playing = true;
     }
     
+    /**
+     * Add a single Track to this Queue
+     * @param t the track to add to this Queue
+     */
     public void addToQueue(Track t){
         queue.put(queue.size()+1, t);
         updateStatefulQueue();
     }
     
+    /**
+     * Add an entire Queue to this Queue.
+     * @param q the Queue to add to this Queue
+     */
     public void addToQueue(Queue q){
         queue.putAll(q);
         updateStatefulQueue();
     }
     
+    /**
+     * Pause the currently playing track. Does nothing when not playing.
+     * @throws NonFatalException when writing to the FFPlay fails
+     */
     public void pause() throws NonFatalException {
         if(!playing){
             return;
         }
-        playing = false;
         try{
             procInput.write("p");
         } catch (IOException ex) {
@@ -289,30 +361,44 @@ public class Player {
             procKill = true;
             throw new NonFatalException("Could not pause FFPlay; Force-quit it", ex);
         }
+        playing = false;
     }
     
+    /**
+     * Resume playing the currently paused track. Does nothing when playing.
+     * @throws NonFatalException when writing to the FFPlay fails
+     */
     public void resume() throws NonFatalException{
         if(playing){
             return;
         }
         try{
             procInput.write("p");
+            playing = true;
         } catch (IOException ex) {
             proc.destroy();
             procKill = true;
             throw new NonFatalException("Could not resume FFPlay; Force-quit it", ex);
         }
-        playing = true;
     }
     
+    /**
+     * Stop the currently playing song by sending an exit signal to FFPlay.
+     * @throws NonFatalException when FFPlay does not stop by itself
+     */
     public void stop() throws NonFatalException {
-        if(!proc.isAlive()){
+        if(!proc.isAlive()) {
+            //If the process has already exited, or FFPlay wasn't started yet
             return;
         }
+        //Try to leverage the threads to exit by themselves
         playing = false;
         procKill = true;
         try{
-            procInput.write("q");
+            //Can't tell a dead process to stop
+            if(proc.isAlive()) {
+                procInput.write("q");
+            }
         } catch (IOException ex) {
             proc.destroy();
             throw new NonFatalException("Could not stop FFPlay; Force-quit it", ex);
@@ -320,11 +406,66 @@ public class Player {
         proc.destroy();
     }
     
+    /**
+     * Handle what happens when a song ends (and FFPlay exits)
+     * @throws FatalException propagated from {@link #playTrack(com.PiJukebox.Track) playTrack}
+     * @throws NonFatalException propagated from {@link #playTrack(com.PiJukebox.Track) playTrack}
+     */
     protected void onSongEnd() throws FatalException, NonFatalException{
         if(repeatOne){
             this.playTrack(currentTrack);
         } else {
             next();
         }
+    }
+    
+    /**
+     * Get the {@link com.PiJukebox.ErrorLogger ErrorLogger} that belongs to this Player.
+     * This method should be the first one called if planning to allow recovery
+     * from a {@link com.PiJukebox.FatalException FatalException}, to ensure retrieval.
+     * @return 
+     */
+    public ErrorLogger getLogger(){
+        return this.log;
+    }
+    
+    /**
+     * Get the current repeat state.
+     * This does NOT give a correct indication of repeatOne.
+     * @return true if in repeat mode, false otherwise.
+     */
+    public boolean getRepeat(){
+        return this.repeat;
+    }
+    
+    /**
+     * Toggles repeat between true and false.
+     * If this turns off repeat, it will also turn off repeatOne.
+     */
+    public void toggleRepeat(){
+        if(repeat){ //If we're going to false, we should still be true
+            if(repeatOne){ //If repeatOne is also true, we flip it
+                toggleRepeatOne();
+            }
+        }
+        this.repeat = !repeat; //Flip repeat, this'll change it both ways.
+    }
+    
+    /**
+     * Get the current repeatOne state.
+     * If {@link #toggleRepeatOne()   this variable's toggle method} has been called at least once,
+     * this is a decent indication of repeat.
+     * @return true if in repeatOne mode, false otherwise.
+     */
+    public boolean getRepeatOne(){
+        return this.repeatOne;
+    }
+    
+    /**
+     * Toggles repeatOne between true and false.
+     * Has no effect on any other value
+     */
+    public void toggleRepeatOne(){
+        this.repeatOne = !repeatOne;
     }
 }
