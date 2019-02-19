0c51e6b74ea5664aa9f95a4e636b61ca1dd653c8
Rutger <rutgermensen@gmail.com>
2019-02-19 12:18:03 +0100

    Refactored TrackController calls

--- Backend/src/main/java/com/pijukebox/controller/TrackController.java
+++ Backend/src/main/java/com/pijukebox/controller/TrackController.java
@@ -10,7 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -38,15 +37,9 @@ public class TrackController {
     @ApiOperation(value = "Get all pertaining to tracks (without relations)", notes = "Filter the returned items using the name parameter")
     public ResponseEntity<List<SimpleTrack>> simpleTracks(@RequestParam(name = "name", required = false) String name) {
         if (name != null && !name.isEmpty()) {
-            if (!trackService.findAllSimpleTrackByName(name).isPresent()) {
-                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-            }
-            return new ResponseEntity<>(trackService.findAllSimpleTrackByName(name).get(), HttpStatus.OK);
+            return new ResponseEntity<>(trackService.findAllSimpleTrackByName(name).getBody(), HttpStatus.OK);
         }
-        if (!trackService.findAllSimpleTrack().isPresent()) {
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-        }
-        return new ResponseEntity<>(trackService.findAllSimpleTrack().get(), HttpStatus.OK);
+        return new ResponseEntity<>(trackService.findAllSimpleTrack().getBody(), HttpStatus.OK);
     }
 
     /**
@@ -65,29 +58,17 @@ public class TrackController {
             if (searchBy != null && !searchBy.isEmpty()) {
                 switch (searchBy.toLowerCase()) {
                     case "genre":
-                        if (!trackService.findAllTracksByGenreName(name).isPresent()) {
-                            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-                        } else {
-                            return new ResponseEntity<>(trackService.findAllTracksByGenreName(name).get(), HttpStatus.OK);
-                        }
+                        return new ResponseEntity<>(trackService.findAllTracksByGenreName(name).getBody(), HttpStatus.OK);
                     case "artist":
-                        if (!trackService.findAllTracksByArtistName(name).isPresent()) {
-                            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-                        } else {
-                            return new ResponseEntity<>(trackService.findAllTracksByArtistName(name).get(), HttpStatus.OK);
-                        }
+                        return new ResponseEntity<>(trackService.findAllTracksByArtistName(name).getBody(), HttpStatus.OK);
                     default:
                         return new ResponseEntity<>("No valid search value. Use 'genre' or 'artist'", HttpStatus.BAD_REQUEST);
                 }
             } else {
-                if (!trackService.findAllTracksByName(name).isPresent()) {
-                    return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-                } else {
-                    return new ResponseEntity<>(trackService.findAllTracksByName(name).get(), HttpStatus.OK);
-                }
+                return new ResponseEntity<>(trackService.findAllTracksByName(name).getBody(), HttpStatus.OK);
             }
         } else {
-            return new ResponseEntity<>(trackService.findAllTracksWithDetails(), HttpStatus.OK);
+            return new ResponseEntity<>(trackService.findAllTracksWithDetails().getBody(), HttpStatus.OK);
         }
     }
 
@@ -102,10 +83,7 @@ public class TrackController {
     @GetMapping("/extended/tracks/{id}")
     @ApiOperation(value = "Get all information pertaining to a certain track (with relations) by its ID")
     public ResponseEntity<Track> trackDetails(@PathVariable Long id) {
-        if (!trackService.findTrackDetailsById(id).isPresent()) {
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-        }
-        return new ResponseEntity<>(trackService.findTrackDetailsById(id).get(), HttpStatus.OK);
+        return new ResponseEntity<>(trackService.findTrackDetailsById(id).getBody(), HttpStatus.OK);
     }
 
     /**
@@ -119,10 +97,7 @@ public class TrackController {
     @GetMapping("/simple/tracks/{id}")
     @ApiOperation(value = "Get all information pertaining to a certain track (without relations) by its ID")
     public ResponseEntity<SimpleTrack> simpleTrack(@PathVariable Long id) {
-        if (!trackService.findTrackDetailsById(id).isPresent()) {
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-        }
-        return new ResponseEntity<>(trackService.findSimpleTrackById(id).get(), HttpStatus.OK);
+        return new ResponseEntity<>(trackService.findSimpleTrackById(id).getBody(), HttpStatus.OK);
     }
 
     /**
@@ -134,12 +109,7 @@ public class TrackController {
     @PostMapping("/simple/tracks")
     @ApiOperation(value = "Add a new track")
     public ResponseEntity<SimpleTrack> addSimpleTrack(@RequestBody SimpleTrack simpleTrack) {
-        try {
-            return new ResponseEntity<>(trackService.addSimpleTrack(simpleTrack), HttpStatus.CREATED);
-        } catch (Exception ex) {
-            ex.printStackTrace();
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Something went wrong!", ex);
-        }
+        return new ResponseEntity<>(trackService.addSimpleTrack(simpleTrack).getBody(), HttpStatus.CREATED);
     }
 
     /**
@@ -152,15 +122,11 @@ public class TrackController {
     @ApiOperation(value = "Update an existing a track")
     public ResponseEntity<SimpleTrack> updateSimpleTrack(@RequestBody SimpleTrack simpleTrack) {
         if (simpleTrack.getId() != null && !simpleTrack.getId().toString().isEmpty()) {
-            if (!trackService.findTrackDetailsById(simpleTrack.getId()).isPresent()) {
-                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-            }
-
-            SimpleTrack simpleTrack1 = trackService.findSimpleTrackById(simpleTrack.getId()).get();
+            SimpleTrack simpleTrack1 = trackService.findSimpleTrackById(simpleTrack.getId()).getBody();
             simpleTrack1.setName(simpleTrack.getName());
             simpleTrack1.setDescription(simpleTrack.getDescription());
             simpleTrack1.setFilename(simpleTrack.getFilename());
-            return new ResponseEntity<>(trackService.addSimpleTrack(simpleTrack1), HttpStatus.CREATED);
+            return new ResponseEntity<>(trackService.addSimpleTrack(simpleTrack1).getBody(), HttpStatus.CREATED);
         }
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
@@ -175,41 +141,26 @@ public class TrackController {
     @PatchMapping("/extended/tracks/{id}/genres/{genreId}")
     @ApiOperation(value = "Add a new genre to an existing track")
     public ResponseEntity<GenreWithTracks> addGenreToTrack(@PathVariable Long id, @PathVariable Long genreId) {
-        if (!trackService.findSimpleTrackById(id).isPresent()) {
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-        }
-        if (!trackService.findTrackByGenreId(genreId).isPresent()) {
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-        }
-        GenreWithTracks genreWithTracks = trackService.findTrackByGenreId(genreId).get();
-        SimpleTrack simpleTrack = trackService.findSimpleTrackById(id).get();
+        GenreWithTracks genreWithTracks = trackService.findTrackByGenreId(genreId).getBody();
+        SimpleTrack simpleTrack = trackService.findSimpleTrackById(id).getBody();
         genreWithTracks.getTracks().add(simpleTrack);
-        return new ResponseEntity<>(trackService.addGenreToTrack(genreWithTracks), HttpStatus.CREATED);
+        return new ResponseEntity<>(trackService.addGenreToTrack(genreWithTracks).getBody(), HttpStatus.CREATED);
     }
 
     @PatchMapping("/extended/tracks/{id}/artists/{artistId}")
     @ApiOperation(value = "Add a new artist to an existing track")
     public ResponseEntity<ArtistWithTracks> addArtistToTrack(@PathVariable Long id, @PathVariable Long artistId) {
-        if (!trackService.findSimpleTrackById(id).isPresent()) {
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-        }
-        if (!trackService.findTrackByArtistId(artistId).isPresent()) {
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-        }
-        ArtistWithTracks genreTrack = trackService.findTrackByArtistId(artistId).get();
-        SimpleTrack simpleTrack = trackService.findSimpleTrackById(id).get();
+        ArtistWithTracks genreTrack = trackService.findTrackByArtistId(artistId).getBody();
+        SimpleTrack simpleTrack = trackService.findSimpleTrackById(id).getBody();
         genreTrack.getTracks().add(simpleTrack);
-        return new ResponseEntity<>(trackService.addArtistToTrack(genreTrack), HttpStatus.CREATED);
+        return new ResponseEntity<>(trackService.addArtistToTrack(genreTrack).getBody(), HttpStatus.CREATED);
     }
 
     @GetMapping("/tracks/search/{searchTerm}")
     @ApiOperation(value = "Search Tracks")
     public ResponseEntity<List<Track>> searchTracks(@PathVariable String searchTerm) {
         if (searchTerm != null && !searchTerm.isEmpty()) {
-            if (!trackService.findAllTracksByName(searchTerm).isPresent()) {
-                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-            }
-            return new ResponseEntity<>(trackService.findAllTracksByName(searchTerm).get(), HttpStatus.OK);
+            return new ResponseEntity<>(trackService.findAllTracksByName(searchTerm).getBody(), HttpStatus.OK);
         }
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
--- Backend/src/main/java/com/pijukebox/service/ITrackService.java
+++ Backend/src/main/java/com/pijukebox/service/ITrackService.java
@@ -4,35 +4,35 @@ import com.pijukebox.model.artist.ArtistWithTracks;
 import com.pijukebox.model.genre.GenreWithTracks;
 import com.pijukebox.model.simple.SimpleTrack;
 import com.pijukebox.model.track.Track;
+import org.springframework.http.ResponseEntity;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface ITrackService {
 
-    List<Track> findAllTracksWithDetails();
+    ResponseEntity<List<Track>> findAllTracksWithDetails();
 
-    Optional<Track> findTrackDetailsById(Long id);
+    ResponseEntity<Track> findTrackDetailsById(Long id);
 
-    Optional<List<SimpleTrack>> findAllSimpleTrack();
+    ResponseEntity<List<SimpleTrack>> findAllSimpleTrack();
 
-    Optional<SimpleTrack> findSimpleTrackById(Long id);
+    ResponseEntity<SimpleTrack> findSimpleTrackById(Long id);
 
-    Optional<List<SimpleTrack>> findAllSimpleTrackByName(String name);
+    ResponseEntity<List<SimpleTrack>> findAllSimpleTrackByName(String name);
 
-    Optional<List<Track>> findAllTracksByName(String name);
+    ResponseEntity<List<Track>> findAllTracksByName(String name);
 
-    Optional<List<ArtistWithTracks>> findAllTracksByArtistName(String name);
+    ResponseEntity<List<ArtistWithTracks>> findAllTracksByArtistName(String name);
 
-    Optional<List<GenreWithTracks>> findAllTracksByGenreName(String name);
+    ResponseEntity<List<GenreWithTracks>> findAllTracksByGenreName(String name);
 
-    SimpleTrack addSimpleTrack(SimpleTrack simpleTrack);
+    ResponseEntity<SimpleTrack> addSimpleTrack(SimpleTrack simpleTrack);
 
-    ArtistWithTracks addArtistToTrack(ArtistWithTracks artistWithTracks);
+    ResponseEntity<ArtistWithTracks> addArtistToTrack(ArtistWithTracks artistWithTracks);
 
-    GenreWithTracks addGenreToTrack(GenreWithTracks genreWithTracks);
+    ResponseEntity<GenreWithTracks> addGenreToTrack(GenreWithTracks genreWithTracks);
 
-    Optional<GenreWithTracks> findTrackByGenreId(Long id);
+    ResponseEntity<GenreWithTracks> findTrackByGenreId(Long id);
 
-    Optional<ArtistWithTracks> findTrackByArtistId(Long id);
+    ResponseEntity<ArtistWithTracks> findTrackByArtistId(Long id);
 }
